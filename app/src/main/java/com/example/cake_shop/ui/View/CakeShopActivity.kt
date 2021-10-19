44cda50a54418018b54efbf27b8d576a90d8a188
Viktor <66387359+ViktorKaderabek@users.noreply.github.com>
2021-10-19 10:59:57 +0200

    Cake_Shop

--- app/src/main/java/com/example/cake_shop/ui/View/CakeShopActivity.kt
+++ app/src/main/java/com/example/cake_shop/ui/View/CakeShopActivity.kt
@@ -1,8 +1,7 @@
 package com.example.cake_shop.ui.View
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.TextUtils
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.cake_shop.R
@@ -11,38 +10,180 @@ import com.example.cake_shop.ui.View.fragments.AboutUsFramgen
 import com.example.cake_shop.ui.View.fragments.AccountFragment
 import com.example.cake_shop.ui.View.fragments.HomeFragment
 import com.example.cake_shop.ui.View.fragments.InfoFragment
+
 @Suppress("DEPRECATION")
 class CakeShopActivity : AppCompatActivity() {
 
-    private lateinit var cakeShopActivityBinding : ActivityCakeShopBinding
+    private lateinit var cakeShopActivityBinding: ActivityCakeShopBinding
+
+    private val homeFragment = HomeFragment()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         cakeShopActivityBinding =
-            DataBindingUtil.setContentView(this,R.layout.activity_cake_shop)
+            DataBindingUtil.setContentView(this, R.layout.activity_cake_shop)
 
-         val homeFragment = HomeFragment()
-         val aboutUsFragment = AboutUsFramgen()
-         val inforFragment = InfoFragment()
-         val accountFragment = AccountFragment()
+
 
         makeCurrentFragment(homeFragment)
 
         cakeShopActivityBinding.bottomNav.setOnNavigationItemSelectedListener {
 
-            when(it.itemId){
-                R.id.ic_home -> makeCurrentFragment(homeFragment)
-                R.id.ic_aboutUs -> makeCurrentFragment(aboutUsFragment)
-                R.id.ic_info -> makeCurrentFragment(inforFragment)
-                R.id.ic_account -> makeCurrentFragment(accountFragment)
+            when (it.itemId) {
+
+                R.id.ic_home ->
+                    if (fragmentManager?.findFragmentById(R.id.ic_home) != null) {
+                        //if the fragment exists, show it.
+                        supportFragmentManager.beginTransaction().apply {
+                            show(HomeFragment())
+                            commit()
+                        }
+                    } else {
+                        //if the fragment does not exist, add it to fragment manager.
+                        supportFragmentManager.beginTransaction().apply {
+                            add(R.id.fl_wrapper, HomeFragment())
+                            commit()
+                        }
+                        if (fragmentManager?.findFragmentById(R.id.ic_aboutUs) != null) {
+                            //if the other fragment is visible, hide it.
+                            supportFragmentManager.beginTransaction().apply {
+                                hide(AboutUsFramgen())
+                                commit()
+                            }
+                        }
+                        if (fragmentManager?.findFragmentById(R.id.ic_info) != null) {
+                            //if the other fragment is visible, hide it.
+                            supportFragmentManager.beginTransaction().apply {
+                                hide(InfoFragment())
+                                commit()
+                            }
+                        }
+                        if (fragmentManager?.findFragmentById(R.id.ic_account) != null) {
+                            //if the other fragment is visible, hide it.
+                            supportFragmentManager.beginTransaction().apply {
+                                hide(AccountFragment())
+                                commit()
+                            }
+                        }
+                    }
+                R.id.ic_aboutUs ->
+                    if (fragmentManager?.findFragmentById(R.id.ic_aboutUs) != null) {
+                        //if the fragment exists, show it.
+                        supportFragmentManager.beginTransaction().apply {
+                            show(AboutUsFramgen())
+                            commit()
+                        }
+                    } else {
+                        //if the fragment does not exist, add it to fragment manager.
+                        supportFragmentManager.beginTransaction().apply {
+                            add(R.id.fl_wrapper, AboutUsFramgen())
+                            commit()
+                        }
+                        if (fragmentManager?.findFragmentById(R.id.ic_home) != null) {
+                            //if the other fragment is visible, hide it.
+                            supportFragmentManager.beginTransaction().apply {
+                                hide(HomeFragment())
+                                commit()
+                            }
+                        }
+                        if (fragmentManager?.findFragmentById(R.id.ic_info) != null) {
+                            //if the other fragment is visible, hide it.
+                            supportFragmentManager.beginTransaction().apply {
+                                hide(InfoFragment())
+                                commit()
+                            }
+                        }
+                        if (fragmentManager?.findFragmentById(R.id.ic_account) != null) {
+                            //if the other fragment is visible, hide it.
+                            supportFragmentManager.beginTransaction().apply {
+                                hide(AccountFragment())
+                                commit()
+                            }
+                        }
+                    }
+                R.id.ic_info ->
+                    if (fragmentManager?.findFragmentById(R.id.ic_info) != null) {
+                        //if the fragment exists, show it.
+                        supportFragmentManager.beginTransaction().apply {
+                            show(InfoFragment())
+                            commit()
+                        }
+                    } else {
+                        //if the fragment does not exist, add it to fragment manager.
+                        supportFragmentManager.beginTransaction().apply {
+                            add(R.id.fl_wrapper, InfoFragment())
+                            commit()
+                        }
+                        if (fragmentManager?.findFragmentById(R.id.ic_aboutUs) != null) {
+                            //if the other fragment is visible, hide it.
+                            supportFragmentManager.beginTransaction().apply {
+                                hide(AboutUsFramgen())
+                                commit()
+                            }
+                        }
+                        if (fragmentManager?.findFragmentById(R.id.ic_home) != null) {
+                            //if the other fragment is visible, hide it.
+                            supportFragmentManager.beginTransaction().apply {
+                                hide(HomeFragment())
+                                commit()
+                            }
+                        }
+                        if (fragmentManager?.findFragmentById(R.id.ic_account) != null) {
+                            //if the other fragment is visible, hide it.
+                            supportFragmentManager.beginTransaction().apply {
+                                hide(AccountFragment())
+                                commit()
+                            }
+                        }
+                    }
+                R.id.ic_account ->
+                    if (fragmentManager?.findFragmentById(R.id.ic_account) != null) {
+                        //if the fragment exists, show it.
+                        supportFragmentManager.beginTransaction().apply {
+                            show(AccountFragment())
+                            commit()
+                        }
+                    } else {
+                        //if the fragment does not exist, add it to fragment manager.
+                        supportFragmentManager.beginTransaction().apply {
+                            add(R.id.fl_wrapper, AccountFragment())
+                            commit()
+                        }
+                        if (fragmentManager?.findFragmentById(R.id.ic_aboutUs) != null) {
+                            //if the other fragment is visible, hide it.
+                            supportFragmentManager.beginTransaction().apply {
+                                hide(AboutUsFramgen())
+                                commit()
+                            }
+                        }
+                        if (fragmentManager?.findFragmentById(R.id.ic_info) != null) {
+                            //if the other fragment is visible, hide it.
+                            supportFragmentManager.beginTransaction().apply {
+                                hide(InfoFragment())
+                                commit()
+                            }
+                        }
+                        if (fragmentManager?.findFragmentById(R.id.ic_home) != null) {
+                            //if the other fragment is visible, hide it.
+                            supportFragmentManager.beginTransaction().apply {
+                                hide(HomeFragment())
+                                commit()
+                            }
+                        }
+                    }
             }
             true
+
         }
 
     }
-    private fun makeCurrentFragment(fragment : Fragment) =
+
+    private fun makeCurrentFragment(fragment: Fragment) =
         supportFragmentManager.beginTransaction().apply {
-        replace(R.id.fl_wrapper, fragment)
-        commit()
-    }
+            replace(R.id.fl_wrapper, fragment)
+            commit()
+        }
+
+
 }
