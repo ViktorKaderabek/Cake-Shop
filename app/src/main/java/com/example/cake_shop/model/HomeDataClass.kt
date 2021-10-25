4d7a42f015a4eb495b7cccaf069772657f88faf7
Viktor <66387359+ViktorKaderabek@users.noreply.github.com>
2021-10-25 10:08:52 +0200

    Cake_shop

--- app/src/main/java/com/example/cake_shop/model/HomeDataClass.kt
+++ app/src/main/java/com/example/cake_shop/model/HomeDataClass.kt
@@ -1,7 +1,7 @@
 package com.example.cake_shop.model
 
 data class HomeDataClass(
-    val title: String,
+    val title: String, //data do recycerlView
     val secondaryTitle: String,
     val image: Int
 ) {
