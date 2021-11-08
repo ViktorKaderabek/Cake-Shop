9d99b7d202ac727cfd5c900f3c3bed54d5763b0a
Viktor <66387359+ViktorKaderabek@users.noreply.github.com>
2021-11-08 14:51:46 +0100

    Cake_shop

--- app/src/main/java/com/example/cake_shop/model/data/dbConnection/ConnectionHelper.kt
+++ app/src/main/java/com/example/cake_shop/model/data/dbConnection/ConnectionHelper.kt
@@ -9,27 +9,26 @@ import java.sql.SQLException
 
 class ConnectionHelper {
 
-
     private var connection: Connection? = null
 
     @SuppressLint("NewApi")
 
     fun getConnection(): Connection {
 
-        val ip = "192.168.0.242" //ip addressa na ktere bezi server
-        val port = "1300" // cislo portu
-        val database = "CakeShopDB" //nazev Databaze
-        val us = "test" //prihlasovaci udaje do Sql Serveru
-        val pass = "1234"//prihlasovaci udaje do Sql Serveru
+        val ip = "192.168.0.242" //ip localhosota
+        val port = "1300" //port localhosta
+        val database = "CakeShopDB" //nazev DB
+        val us = "test" // username k DB
+        val pass = "1234" // heslo k DB
         val url =
-            "jdbc:jtds:sqlserver://$ip:$port/$database" //url pres ktery se prihlasuje do dbs nemeni se!!
+            "jdbc:jtds:sqlserver://$ip:$port/$database"
 
         val policy: StrictMode.ThreadPolicy =
             StrictMode.ThreadPolicy.Builder().permitAll().build()
 
         StrictMode.setThreadPolicy(policy)
 
-        try {//zkousi se pripojit do me dbs
+        try { //zkousi se pripojit k DB
             Class.forName("net.sourceforge.jtds.jdbc.Driver")
             connection = DriverManager.getConnection(url, us, pass)
         } catch (exception: ClassNotFoundException) {
@@ -40,8 +39,6 @@ class ConnectionHelper {
             Log.e("Error :", exception.message.toString())
         }
 
-        return connection!!
-
+        return connection!! //pokud se pripoji tak vrati pripojeni
     }
-
 }
