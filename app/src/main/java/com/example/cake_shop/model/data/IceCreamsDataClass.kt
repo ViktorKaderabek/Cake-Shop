d569299160d1f76ca99714b5f6bfaad3bb5d1113
Viktor <66387359+ViktorKaderabek@users.noreply.github.com>
2021-11-14 18:06:56 +0100

    Cake_shop

--- app/src/main/java/com/example/cake_shop/model/data/IceCreamsDataClass.kt
+++ app/src/main/java/com/example/cake_shop/model/data/IceCreamsDataClass.kt
@@ -2,10 +2,11 @@ package com.example.cake_shop.model.data
 
 import android.graphics.Bitmap
 
-data class IceCreamsDataClass(//data pro recyclerView
-    val name: String,
-    val popis: String,
-    val alergeny: String,
-    val cena: String,
-    val picture: Bitmap
-)
+data class IceCreamsDataClass(
+//data pro recyclerView
+    val name : String,
+    val popis : String,
+    val alergeny : String,
+    val cena : String,
+    val picture : Bitmap,
+		         )
--- app/src/main/java/com/example/cake_shop/ui/ViewModel/HomeFragmentViewModel.kt
+++ app/src/main/java/com/example/cake_shop/ui/ViewModel/HomeFragmentViewModel.kt
@@ -5,10 +5,11 @@ import com.example.cake_shop.ui.adapter.HomeFastAdapter
 import com.mikepenz.fastadapter.FastAdapter
 import com.mikepenz.fastadapter.adapters.ItemAdapter
 
-class HomeFragmentViewModel : ViewModel() {
-
-    val itemAdapter =
-        ItemAdapter<HomeFastAdapter>()
-    val fastAdapter =
-        FastAdapter.with(itemAdapter)
+class HomeFragmentViewModel : ViewModel()
+{
+     
+     val itemAdapter =
+	ItemAdapter<HomeFastAdapter>()
+     val fastAdapter =
+	FastAdapter.with(itemAdapter)
 }
